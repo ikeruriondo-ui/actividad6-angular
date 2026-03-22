c7482430d9089ee55e50164379af89b123d0c236
ikeruriondo-ui <ikeruriondo@gmail.com>
2026-03-22 16:17:35 +0100

    Corrección de tipos id en servicio y home

--- src/app/components/home/home.ts
+++ src/app/components/home/home.ts
@@ -1,8 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
+import { User } from '../../interfaces/user';
+import { UsersService } from '../../services/users';
 
 @Component({
   selector: 'app-home',
+  standalone: true,
+  imports: [CommonModule, RouterLink],
   templateUrl: './home.html',
   styleUrl: './home.css'
 })
-export class HomeComponent {}
+export class HomeComponent implements OnInit {
+  users: User[] = [];
+  private usersService = inject(UsersService);
+
+  ngOnInit(): void {
+    this.usersService.getUsers().subscribe({
+      next: (response: any) => {
+        this.users = response.results ?? [];
+      },
+      error: (error: any) => {
+        console.error('Error al cargar usuarios', error);
+      }
+    });
+  }
+
+  onDelete(id: number | undefined, name: string): void {
+    if (!id) return;
+
+    const ok = confirm(`¿Seguro que quieres eliminar a ${name}?`);
+
+    if (ok) {
+      this.usersService.deleteUser(id).subscribe({
+        next: () => {
+          this.users = this.users.filter(user => user.id );
+          alert('Usuario eliminado correctamente');
+        },
+        error: (error: any) => {
+          console.error('Error al eliminar usuario', error);
+          alert('No se pudo eliminar el usuario');
+        }
+      });
+    }
+  }
+}
--- src/app/services/users.ts
+++ src/app/services/users.ts
@@ -1,8 +1,32 @@
-import { Injectable } from '@angular/core';
+import { Injectable, inject } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { User } from '../interfaces/user';
 
 @Injectable({
-  providedIn: 'root',
+  providedIn: 'root'
 })
-export class Users {
-  
+export class UsersService {
+  private http = inject(HttpClient);
+  private baseUrl = 'https://peticiones.online/api/users';
+
+  getUsers(): Observable<any> {
+    return this.http.get<any>(this.baseUrl);
+  }
+
+  getUserById(id: string): Observable<any> {
+    return this.http.get<any>(`${this.baseUrl}/${id}`);
+  }
+
+  createUser(user: User): Observable<any> {
+    return this.http.post<any>(this.baseUrl, user);
+  }
+
+  updateUser(id: string, user: User): Observable<any> {
+    return this.http.put<any>(`${this.baseUrl}/${id}`, user);
+  }
+
+  deleteUser(id: number): Observable<any> {
+  return this.http.delete<any>(`${this.baseUrl}/${id}`);
+  }
 }
