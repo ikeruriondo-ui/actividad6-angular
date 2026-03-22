ef50daf08355405484ba5dd994bdb4251d9cefec
ikeruriondo-ui <ikeruriondo@gmail.com>
2026-03-22 18:24:11 +0100

    Interfaz final home con acciones CRUD completas

--- src/app/components/home/home.ts
+++ src/app/components/home/home.ts
@@ -1,17 +1,20 @@
 import { Component, OnInit, inject, ChangeDetectorRef } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
+
 import { User } from '../../interfaces/user';
 import { UsersService } from '../../services/users';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterLink],
   templateUrl: './home.html',
   styleUrl: './home.css'
 })
 export class HomeComponent implements OnInit {
   users: User[] = [];
+
   private usersService = inject(UsersService);
   private cdr = inject(ChangeDetectorRef);
 
@@ -23,6 +26,24 @@ export class HomeComponent implements OnInit {
       },
       error: (error: any) => {
         console.error('Error al cargar usuarios', error);
+      }
+    });
+  }
+
+  onDelete(id: number, name: string): void {
+    const ok = confirm(`¿Seguro que quieres eliminar a ${name}?`);
+
+    if (!ok) return;
+
+    this.usersService.deleteUser(id).subscribe({
+      next: () => {
+        this.users = this.users.filter(user => user.id !== id);
+        this.cdr.detectChanges();
+        alert('Usuario eliminado correctamente');
+      },
+      error: (error: any) => {
+        console.error('Error al eliminar usuario', error);
+        alert('No se pudo eliminar el usuario');
       }
     });
   }
